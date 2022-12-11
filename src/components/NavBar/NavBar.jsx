756b8fcc5109a5a51d846bbbffe700de5c14446c
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-12-11 18:15:50 +0100

    add - border bottom navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = ({ navigation }) => {
     const bg = useColorModeValue('primary.50', 'primary.1000')
     const colorText = useColorModeValue('black', 'white')
     return (
-        <Box pl={4} pr={4} height={100} bg={bg}>
+        <Box pl={4} pr={4} height={100} bg={bg} borderBottomWidth="0.2" borderBottomColor="gray.300">
             <Box pt={1} pb={1} mt={60}>
                 <HStack justifyContent="space-between">
                     <TouchableOpacity onPress={() => handlePressMenuButton()}>
