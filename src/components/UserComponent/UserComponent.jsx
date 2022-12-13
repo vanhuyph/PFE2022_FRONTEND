dd372b647a23065d8112d5033a672ea7c4ffc126
vanhuyph <vanhuypham26@gmail.com>
2022-12-13 14:32:58 +0100

    add - end of search functionnality

--- src/components/UserComponent/UserComponent.jsx
+++ src/components/UserComponent/UserComponent.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Box, Heading, HStack, Button, Text } from 'native-base'
-import { TouchableOpacity } from 'react-native'
+import React from 'react';
+import { Box, Heading, HStack, Button, Text } from 'native-base';
+import { TouchableOpacity } from 'react-native';
 
-const UserComponent = () => {
-    return (
-        <Box p={6} borderBottomWidth="0.2" borderBottomColor="gray.300" _dark={{ bg: 'primary.1000' }} _light={{ bg: 'primary.50' }}>
-            <HStack space={5} justifyContent="space-around">
-                <TouchableOpacity >
-                    <Box my="auto">
-                        <Heading size="sm">Nickname</Heading>
-                    </Box>
-                </TouchableOpacity>
-                <Button><Text color="white" bold>Follow</Text></Button>
-            </HStack>
-        </Box>
-    )
-}
+const UserComponent = ({ user }) => {
+  return (
+    <Box
+      p={6}
+      borderBottomWidth="0.2"
+      borderBottomColor="gray.300"
+      _dark={{ bg: 'primary.1000' }}
+      _light={{ bg: 'primary.50' }}
+    >
+      <HStack space={5} justifyContent="space-around">
+        <TouchableOpacity>
+          <Box my="auto">
+            <Heading size="sm">{user.username}</Heading>
+          </Box>
+        </TouchableOpacity>
+        <Button>
+          <Text color="white" bold>
+            Follow
+          </Text>
+        </Button>
+      </HStack>
+    </Box>
+  );
+};
 
-export default UserComponent
+export default UserComponent;
