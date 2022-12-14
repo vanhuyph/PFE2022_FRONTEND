650bf3895e41fdd33f5520c8380b6c97b0338bac
vanhuyph <vanhuypham26@gmail.com>
2022-12-14 14:51:37 +0100

    add - follow + list comments functionnalities

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,41 +1,57 @@
 import React, { useContext, useState } from 'react';
-import { VStack, Box, Heading, useColorModeValue, HStack, Button, Text } from 'native-base';
+import {
+  VStack,
+  Box,
+  Heading,
+  useColorModeValue,
+  HStack,
+  Button,
+  Text,
+} from 'native-base';
 import { AuthContext } from '../../contexts/AuthContext';
+import SubscriptionService from '../../services/SubscriptionService';
 
 const ProfileCard = ({ user }) => {
   const bg = useColorModeValue('primary.50', 'primary.1000');
-  const { userInfo } = useContext(AuthContext)
-  const [followPressed, setFollowPressed] = useState(false)
+  const { userInfo, userToken } = useContext(AuthContext);
+  const [followPressed, setFollowPressed] = useState(false);
   let button;
+
+  let dataToSend = {
+    user: userInfo.id,
+    subscription: user.id,
+  };
 
   const onPressFollow = () => {
     if (!followPressed) {
       //l'utilisateur doit se follow
+      SubscriptionService.createSubscription(dataToSend, userToken)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
     } else {
       //l'utilisateur doit se unfollow
+      SubscriptionService.deleteSubscription(dataToSend, userToken)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
     }
-    setFollowPressed(!followPressed)
-  }
+    setFollowPressed(!followPressed);
+  };
 
   if (user.id === userInfo.id) {
-    button = <Box></Box>
+    button = <Box></Box>;
   } else {
     if (!followPressed) {
-      button = <Button onPress={onPressFollow}>Follow</Button>
+      button = <Button onPress={onPressFollow}>Follow</Button>;
     } else {
-      button = <Button onPress={onPressFollow}>Unfollow</Button>
+      button = <Button onPress={onPressFollow}>Unfollow</Button>;
     }
   }
-
-
 
   return (
     <Box bg={bg} pt={8} pb={8}>
       <VStack>
         <HStack justifyContent="space-around">
-          <Heading size={'2xl'}>
-            {user.username}
-          </Heading>
+          <Heading size={'2xl'}>{user.username}</Heading>
           {button}
         </HStack>
         <HStack pl={5} pt={10} space={5}>
@@ -43,7 +59,7 @@ const ProfileCard = ({ user }) => {
           <Text>{user.followers_count} Followers</Text>
         </HStack>
       </VStack>
-    </Box >
+    </Box>
   );
 };
 
