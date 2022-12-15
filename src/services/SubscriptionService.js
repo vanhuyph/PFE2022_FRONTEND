0ab8af7b6302fd0c716b76f67430d1b3599f3d5a
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-12-15 20:43:47 +0100

    add - config to backend

--- src/services/SubscriptionService.js
+++ src/services/SubscriptionService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { BASE_URL } from '@env'
+//import { BASE_URL } from '@env'
+import { BASE_URL } from '../config'
 const baseUrl = `${BASE_URL}/subscriptions`;
 
 const createSubscription = (newObject, userToken) => {
